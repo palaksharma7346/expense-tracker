0d5606ad097a2f97f8dedeaf51c06c2bff611e96
palaksharma7346 <palak412005sharma@gmail.com>
2025-07-25 22:20:50 +0530

    commit

--- frontend/expenset/expensetracker/src/pages/dashboard/Home.jsx
+++ frontend/expenset/expensetracker/src/pages/dashboard/Home.jsx
@@ -1,10 +1,13 @@
 import React from 'react'
+import DashboardLayout from '../../compnents/layouts/DashboardLayout.jsx'
+import { useUserAuth } from '../../hooks/useUserAuth.jsx';
 
 const Home = () => {
+  useUserAuth();
   return (
    <DashboardLayout activeMenu = "Dashboard">
-    <div>
-      
+    <div className='my-5 mx-auto'>
+      home
     </div>
    </DashboardLayout>
   )
