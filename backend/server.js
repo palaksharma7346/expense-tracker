713c3d3737f01459a7bd202387eb643b5cffea89
palaksharma7346 <palak412005sharma@gmail.com>
2026-05-18 00:12:12 +0530

    Fixed root route order

--- backend/server.js
+++ backend/server.js
@@ -8,7 +8,9 @@ const incomeRoutes = require("./routes/incomeRoutes");
 const expenseRoutes = require("./routes/expenseRoutes");
 const dashboardRoutes = require("./routes/dashboardRoutes");
 const app = express();
-
+app.get("/", (req, res) => {
+  res.send("Expense Tracker API is running...");
+});
 //middleware to handle cors
 app.use(cors({
     origin: process.env.CLIENT_URL || "*",
@@ -31,8 +33,6 @@ app.use((req, res) => {
     res.status(404).json({ message: "Route not found" });
 });
 
-app.get("/", (req, res) => {
-  res.send("Expense Tracker API is running...");
-});
+
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => console.log(`Server is running on port ${PORT}`));
