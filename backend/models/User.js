f4ed296c9b6e3b2d408b1dea3a63ecec07de6e75
palak sharma <palak412005sharma@gmail.com>
2026-05-19 17:17:05 +0530

    Refactor pre-save hook for password hashing
    Refactor password hashing in userSchema pre-save hook to remove next() callback.

--- backend/models/User.js
+++ backend/models/User.js
@@ -22,12 +22,12 @@ const userSchema = new mongoose.Schema({
     },
 }, { timestamps: true });
 
-userSchema.pre('save', async function (next) {
+userSchema.pre('save', async function () {
     if (!this.isModified('password')) {
-        return next();
+        return;
     }
+
     this.password = await bcrypt.hash(this.password, 10);
-    next();
 });
 
 userSchema.methods.comparePassword = async function (candidatePassword) {
